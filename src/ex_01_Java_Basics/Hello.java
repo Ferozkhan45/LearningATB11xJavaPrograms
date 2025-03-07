447433e85607137a206e8ead6d4020b57757b0c4
Feroz khan <ferozkhan1786@gmail.com>
2025-03-07 08:29:39 +0530

    07-03-25 Pusing Helo world code

--- src/ex_01_Java_Basics/Hello.java
+++ src/ex_01_Java_Basics/Hello.java
@@ -1,10 +1,13 @@
 package ex_01_Java_Basics;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Hello
 {
 
     public static void main(String[] args) {
       System.out.println("Hello World");
+      System.out.println("My name is feroz");
     }
 
 
