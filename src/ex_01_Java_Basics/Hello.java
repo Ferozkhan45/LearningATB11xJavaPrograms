91129a166b3ceb61b9656888c571b073ba785d08
Feroz khan <ferozkhan1786@gmail.com>
2025-03-07 08:44:25 +0530

    07-03-25 Pusing Helo world code great

--- src/ex_01_Java_Basics/Hello.java
+++ src/ex_01_Java_Basics/Hello.java
@@ -1,13 +1,13 @@
 package ex_01_Java_Basics;
 
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-import java.sql.SQLOutput;
+
 
 public class Hello
 {
 
     public static void main(String[] args) {
         System.out.println("Great");
+        System.out.println("Hello World");
     }
 
 
