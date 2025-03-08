1f5f4ee6021275a9308b7d81255e882d2ae39164
Feroz khan <ferozkhan1786@gmail.com>
2025-03-08 12:48:04 +0530

    08-03-25 Pusing Helo world code Name change

---
+++ src/ex_02_Java_Basics/practise_print.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics;
+
+public class practise_print
+{
+
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("Today is Saturday");
+
+    }
+
+
+
+}
