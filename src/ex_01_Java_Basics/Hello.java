8d87860717f23e65426ac85d5a775ef4ae3e17a2
Feroz khan <ferozkhan1786@gmail.com>
2025-03-08 12:43:49 +0530

    08-03-25 Pusing Helo world code Name change

--- src/ex_01_Java_Basics/Hello.java
+++ src/ex_01_Java_Basics/Hello.java
@@ -1,13 +1,10 @@
 package ex_01_Java_Basics;
 
-
-
 public class Hello
 {
 
     public static void main(String[] args) {
-        System.out.println("Great");
-        System.out.println("Hello World");
+        System.out.println("Im Feroz khan");
     }
 
 
